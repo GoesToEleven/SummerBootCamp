6990bf847c3e8352a6655dcea8075c848f1e89ee
GoesToEleven <tuddleymc@gmail.com>
2015-06-12 10:21:51 -0700

    adds modified instructions

--- javascript/10/03_gulp_again/gulpfile.js
+++ javascript/10/03_gulp_again/gulpfile.js
@@ -1,5 +1,13 @@
 var gulp = require('gulp');
+var webserver = require('gulp-webserver');
 
-gulp.task('default', function() {
-    // place code for your default task here
+gulp.task('default', ['webserver']);
+
+gulp.task('webserver', function() {
+    gulp.src('builds/development')
+        .pipe(webserver({
+            livereload: true,
+            directoryListing: false,
+            open: true
+        }));
 });
