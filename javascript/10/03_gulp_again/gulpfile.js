e2a1dd384cbcb1069da475fc12432c48a1a01af1
GoesToEleven <tuddleymc@gmail.com>
2015-06-12 10:33:28 -0700

    adds gulp-sass

--- javascript/10/03_gulp_again/gulpfile.js
+++ javascript/10/03_gulp_again/gulpfile.js
@@ -1,13 +1,24 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 var webserver = require('gulp-webserver');
 
-gulp.task('default', ['webserver']);
+gulp.task('default', ['webserver', 'sass']);
 
-gulp.task('webserver', function() {
+gulp.task('webserver', function () {
     gulp.src('builds/development')
         .pipe(webserver({
             livereload: true,
             directoryListing: false,
             open: true
         }));
+});
+
+gulp.task('sass', function () {
+    gulp.src('./sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css'));
+});
+
+gulp.task('sass:watch', function () {
+    gulp.watch('./sass/**/*.scss', ['sass']);
 });
