d60b84835055e6986c746fdb9dd51ef3ee4a420d
GoesToEleven <tuddleymc@gmail.com>
2015-06-22 09:12:18 -0700

    wireframing componentization

---
+++ web_components/01/01_andrews_web-components-training-exercises/nesting-components/accordion_component/script.js
@@ -0,0 +1,15 @@
+var accordions = Array.prototype.slice.call(document.querySelectorAll('.accordion-component'));
+accordions.forEach(function(accordion) {
+  var allContent = Array.prototype.slice.call(accordion.querySelectorAll('.content'));
+  var sections = Array.prototype.slice.call(accordion.querySelectorAll('.section'));
+  sections.forEach(function(section) {
+    var content = section.querySelector('.content');
+    section.querySelector('h2').addEventListener('click', function() {
+      var closed = content.classList.contains('closed');
+      allContent.forEach(function(content) {
+        content.classList.add('closed');
+      });
+      closed ? content.classList.remove('closed') : null;
+    });
+  });
+});
