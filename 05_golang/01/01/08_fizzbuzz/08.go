8b3c620114e7e2d4f76d76a61e7726fc3ca442de
GoesToEleven <tuddleymc@gmail.com>
2015-07-06 16:28:07 -0700

    fizzbuzz

---
+++ 05_golang/01/01/08_fizzbuzz/08.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	for i := 0; i <= 100; i++ {
+		if i%5 == 0 && i%3 == 0 {
+			fmt.Println(i, " -- FizzBuzz")
+		} else if i%3 == 0 {
+			fmt.Println(i, " -- FIZZ")
+		} else if i%5 == 0 {
+			fmt.Println(i, " -- BUZZ")
+		} else {
+			fmt.Println(i)
+		}
+	}
+}
+
+/*
+
+Write a program that prints the numbers from 1 to 100.
+But for multiples of three print "Fizz" instead of the number
+and for the multiples of five print "Buzz".
+For numbers which are multiples of both three and five print "FizzBuzz".
+
+*/
