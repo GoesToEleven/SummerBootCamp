0bccea71b68b981f75dc192b0c7e84ee43131629
GoesToEleven <tuddleymc@gmail.com>
2015-07-07 16:45:13 -0700

    fixed reverses slice

--- 05_golang/01/02/16_reverses_slice/main.go
+++ 05_golang/01/02/16_reverses_slice/main.go
@@ -3,12 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	zs := [1,2,3,4,5,]
-	reverse()
+	zs := []int{1, 2, 3, 4, 5}
+	fmt.Println(reverse(zs))
 }
 
 func reverse(xs []int) []int {
-	ys := make([]int, (len(xs)-1))
+	ys := make([]int, len(xs))
 	for k, v := range xs {
 		ys[len(xs)-(k+1)] = v
 	}
