c4df2f99ff9563504d78246dfa374c3fe7c0ad2b
GoesToEleven <tuddleymc@gmail.com>
2015-07-08 14:37:58 -0700

    better comments

--- 05_golang/01/03/05_swap-two_pointers/main.go
+++ 05_golang/01/03/05_swap-two_pointers/main.go
@@ -3,12 +3,16 @@ package main
 import "fmt"
 
 func swap(x, y *int) {
-//	a := *x
-//	b := *y
-//	*x = b
-//	*y = a
-	*x = *y
-	*y = *x
+	// this doesn't work:
+	//		*x = *y
+	//		*y = *x
+	// this works:
+	//		a := *x
+	//		b := *y
+	//		*x = b
+	//		*y = a
+	// this is better:
+	*x, *y = *y, *x
 }
 
 func main() {
@@ -16,13 +20,12 @@ func main() {
 	y := 2
 	fmt.Println("x", x, &x)
 	fmt.Println("y", y, &y)
-	swap(&x,&y)
+	swap(&x, &y)
 	fmt.Println("x", x, &x)
 	fmt.Println("y", y, &y)
-//	m := 2
-//	m = m + 1
+	//	m := 2
+	//	m = m + 1
 }
-
 
 /*
 Write a program that can swap two integers
