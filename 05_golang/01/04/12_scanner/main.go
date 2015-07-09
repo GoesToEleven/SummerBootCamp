84378271cf780412f61dfc24be0e717d2481d2e1
GoesToEleven <tuddleymc@gmail.com>
2015-07-09 11:14:52 -0700

    string conversion challenge

--- 05_golang/01/04/12_scanner/main.go
+++ 05_golang/01/04/12_scanner/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -17,6 +18,8 @@ func main() {
 	scanner := bufio.NewScanner(srcFile)
 	for scanner.Scan() {
 		line := scanner.Text()
-		fmt.Println(">>>", line)
+//		fmt.Println(">>>", line)
+//		fmt.Println(strings.ToUpper(line[0:1]) + strings.ToLower(line[1:]))
+		fmt.Println(strings.ToUpper(string(line[0])) + strings.ToLower(line[1:]))
 	}
 }
