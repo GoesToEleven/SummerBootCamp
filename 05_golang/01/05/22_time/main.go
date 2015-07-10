8fb8fd8524adf6d3e9e087213b64b292f9351791
GoesToEleven <tuddleymc@gmail.com>
2015-07-10 16:28:23 -0700

    time stuff

---
+++ 05_golang/01/05/22_time/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"time"
+	"fmt"
+)
+
+func main() {
+	var date1 string
+	fmt.Print("Enter your birthday: ")
+	fmt.Scanln(&date1)
+	date2 := time.Now()
+
+	fmt.Println("Enter your social security number: ")
+
+
+}
