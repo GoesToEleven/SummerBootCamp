75f68f4fa384e005b5d4cef3871b595406d0f442
GoesToEleven <tuddleymc@gmail.com>
2015-07-10 08:37:23 -0700

    golang teereader

---
+++ 05_golang/01/05/01_02_caleb_tee-reader/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"io"
+	"os"
+)
+
+func main() {
+	src, err := os.Open("src-file")
+	if err != nil {
+		panic(err)
+	}
+	defer src.Close()
+
+	dst1, err := os.Create("dst1-file")
+	if err != nil {
+		panic(err)
+	}
+	defer dst1.Close()
+
+	dst2, err := os.Create("dst2-file")
+	if err != nil {
+		panic(err)
+	}
+	defer dst2.Close()
+
+	rdr := io.TeeReader(src, dst1)
+	rdr = io.TeeReader(rdr, os.Stdout)
+
+	io.Copy(dst2, rdr)
+
+}
+
+/*
+WHAT TEEREADER DOES....
+
+
+func teeReader(rdr io.Reader) {
+	bs := make([]byte, 10)
+	rdr.Read(bs)
+	w.Write(bs)
+	return bs
+
+
+}
+
+ */
