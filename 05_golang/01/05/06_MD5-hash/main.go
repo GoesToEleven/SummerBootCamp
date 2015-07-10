41f8d906f1282aa701d64e82b563fd2a425cd25c
GoesToEleven <tuddleymc@gmail.com>
2015-07-10 11:07:06 -0700

    mas md5 hash golang

--- 05_golang/01/05/06_MD5-hash/main.go
+++ 05_golang/01/05/06_MD5-hash/main.go
@@ -11,7 +11,8 @@ import (
 
 func main() {
 
-	// open file
+	// one way
+	// reads bytes on the fly
 	f, err := os.Open("../resources/table.csv")
 	if err != nil {
 		log.Fatalln("couldn't open file", err.Error())
@@ -20,9 +21,14 @@ func main() {
 
 	h := md5.New()
 	io.Copy(h, f)
-	fmt.Printf("The hash (sum) is: %x", h.Sum(nil))
+	fmt.Printf("The hash (sum) is: %x\n", h.Sum(nil))
 
 	// or this way
-
-
+	// but reads all the bytes at once, then does it
+	f.Seek(0,0)
+	bs, err := ioutil.ReadAll(f)
+	if err != nil {
+		log.Fatalln("readall didn't read well", err.Error())
+	}
+	fmt.Printf("The hash (sum) is: %x\n", md5.Sum(bs))
 }
