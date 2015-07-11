2c3f72ce109c2114d2edf30478e2a510ce5e423b
GoesToEleven <tuddleymc@gmail.com>
2015-07-10 17:07:08 -0700

    end of friday

--- 05_golang/01/05/22_time/main.go
+++ 05_golang/01/05/22_time/main.go
@@ -3,15 +3,48 @@ package main
 import (
 	"time"
 	"fmt"
+	"reflect"
 )
 
 func main() {
-	var date1 string
-	fmt.Print("Enter your birthday: ")
-	fmt.Scanln(&date1)
-	date2 := time.Now()
+	var userBday string
+	fmt.Print("Enter your birthday (month/day/year): ")
+	fmt.Scanln(&userBday)
 
+	const shortForm = "01/02/2006"
+	date1, _ := time.Parse(shortForm, userBday)
+	fmt.Println(date1)
+
+	date2 := time.Now()
+	difference := date2.Sub(date1)
+
+//	fmt.Println(difference)
+//	fmt.Println(reflect.TypeOf(difference))
+	fmt.Println(difference.Hours())
+	fmt.Println(difference.Minutes())
+
+
+	time.Sleep(2000 * time.Millisecond)
 	fmt.Println("Enter your social security number: ")
+	time.Sleep(1200 * time.Millisecond)
+	fmt.Println("And your credit card number: ")
+	time.Sleep(1000 * time.Millisecond)
+	fmt.Println("And your bank account: ")
+	time.Sleep(900 * time.Millisecond)
+	fmt.Println("Just kidding.")
+	time.Sleep(500 * time.Millisecond)
+	fmt.Println("You've been alive ...")
+	time.Sleep(300 * time.Millisecond)
+	fmt.Println(difference.Seconds(),"seconds")
+	time.Sleep(300 * time.Millisecond)
+	fmt.Println(difference.Minutes(),"minutes")
+	time.Sleep(300 * time.Millisecond)
+	fmt.Println(difference.Hours()/24,"hours")
+	time.Sleep(300 * time.Millisecond)
+	fmt.Println(difference.Hours()/24/30,"days")
+	time.Sleep(300 * time.Millisecond)
+	fmt.Println(difference.Hours()/24,"months")
+
 
 
 }
