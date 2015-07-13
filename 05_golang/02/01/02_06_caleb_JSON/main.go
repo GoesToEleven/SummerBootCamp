9f0013e3aeba9430787c1ffc40c5f66441750ccb
GoesToEleven <tuddleymc@gmail.com>
2015-07-13 09:10:53 -0700

    type casting

---
+++ 05_golang/02/01/02_06_caleb_JSON/main.go
@@ -0,0 +1,27 @@
+package main
+import (
+	"encoding/json"
+	"fmt"
+)
+
+func main() {
+	jsonData := `
+	{
+	100
+	}
+	`
+
+	var obj interface{}
+
+	err := json.Unmarshal([]byte(jsonData), &obj)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(obj)
+
+	fmt.Printf("%T", obj)
+
+	x := 100 + obj.(float64)
+
+	fmt.Println(x)
+}
