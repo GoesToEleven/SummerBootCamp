f3ffb51f33a039bed105a3ed81f7ebb9c7d80b57
GoesToEleven <tuddleymc@gmail.com>
2015-07-13 09:01:12 -0700

    json

---
+++ 05_golang/02/01/02_03_caleb_JSON/main.go
@@ -0,0 +1,24 @@
+package main
+import (
+	"encoding/json"
+	"fmt"
+)
+
+func main() {
+	jsonData := `
+	{
+	"name": "Todd McLeod",
+	"age": 44
+	}
+	`
+
+	type Anything interface{}
+
+	var obj map[string]Anything
+
+	err := json.Unmarshal([]byte(jsonData), &obj)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(obj)
+}
