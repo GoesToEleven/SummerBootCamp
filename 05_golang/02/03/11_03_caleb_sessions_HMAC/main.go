9d217e2561ef788f67745c5aed54859b5d497448
GoesToEleven <tuddleymc@gmail.com>
2015-07-15 13:29:26 -0700

    mas sessions

--- 05_golang/02/03/11_03_caleb_sessions_HMAC/main.go
+++ 05_golang/02/03/11_03_caleb_sessions_HMAC/main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"fmt"
 	"io"
 	"net/http"
 )
+
+func getCode(data string) string {
+	h := hmac.New(sha256.New, []byte("ourkey"))
+	io.WriteString(h, data)
+	return fmt.Sprintf("%x", h.Sum(nil))
+
+}
 
 func main() {
 
@@ -21,14 +31,22 @@ func main() {
 			cookie.Value = req.FormValue("email")
 		}
 
+		code := getCode(cookie.Value)
+		cookie.Value = code + "|" + cookie.Value
+
+		// this doesn't run
+		// need more code added to work
+		// just shown for example of how to do auth with HMAC
+
 		http.SetCookie(res, cookie)
 
 		io.WriteString(res, `<!DOCTYPE html>
 <html>
   <body>
-    <form>
+    <form method="POST">
     `+cookie.Value+`
       <input type="email" name="email">
+      <input type="password" name="password">
       <input type="submit">
     </form>
   </body>
@@ -37,3 +55,5 @@ func main() {
 	})
 	http.ListenAndServe(":9000", nil)
 }
+
+
