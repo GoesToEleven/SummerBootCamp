9ef134e5f0494fb52882b3ba9bb5f662ea45d71f
GoesToEleven <tuddleymc@gmail.com>
2015-07-15 10:36:58 -0700

    form and url data caleb

---
+++ 05_golang/02/03/06_01_caleb_URL-values/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+	"io"
+)
+
+func main() {
+	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
+		key := "q"
+		val := req.URL.Query().Get(key)
+		io.WriteString(res, "Do my search:" + val)
+	})
+	http.ListenAndServe(":9000", nil)
+}
+
+
