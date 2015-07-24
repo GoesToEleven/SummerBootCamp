c9088b439ba5b5e863053f77471e3d570efe4017
GoesToEleven <tuddleymc@gmail.com>
2015-07-24 08:20:57 -0700

    changes routes

--- 05_golang/03/05/01_AE-search_movie-data/routes.go
+++ 05_golang/03/05/01_AE-search_movie-data/routes.go
@@ -6,8 +6,6 @@ import (
 
 func init() {
 	http.HandleFunc("/", handleIndex)
-	http.HandleFunc("/put", handlePut)
-	http.HandleFunc("/get", handleGet)
 	http.Handle("/favicon.ico", http.NotFoundHandler())
 	http.Handle("/public/", http.StripPrefix("/public", http.FileServer(http.Dir("public/"))))
 }
