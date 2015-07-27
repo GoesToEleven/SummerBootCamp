196ac2d27832992507aec80744e43ef52f88329e
GoesToEleven <tuddleymc@gmail.com>
2015-07-27 12:00:09 -0700

    going to lunch

--- 05_golang/04/01/03_02_GCS-files_sessions/handlers.go
+++ 05_golang/04/01/03_02_GCS-files_sessions/handlers.go
@@ -1,8 +1,10 @@
 package GCS
 
 import (
-	"google.golang.org/appengine"
+	//	"google.golang.org/appengine"
+	"github.com/nu7hatch/gouuid"
 	"net/http"
+	"google.golang.org/appengine/memcache"
 )
 
 func index(res http.ResponseWriter, req *http.Request) {
@@ -14,6 +16,25 @@ func index(res http.ResponseWriter, req *http.Request) {
 	err := tpls.ExecuteTemplate(res, "index.html", nil)
 	if err != nil {
 		http.Error(res, err.Error(), 500)
+	}
+
+	if req.Method == "POST" {
+		// set cookie
+		sessionID, _ := uuid.NewV4()
+
+		cookie := &http.Cookie{
+			Name:  "Session",
+			Value: sessionID.String(),
+		}
+		http.SetCookie(res, cookie)
+		// set memcache
+		item1 := &memcache.Item{
+			Key:   sessionID.String(),
+			Value: []byte("bar"), // TODO: Get Value From FORM
+		}
+		if err := memcache.Set(c, item1); err != nil {
+			return err
+		}
 	}
 }
 
